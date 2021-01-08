3ad19aae010b0221015c9b259c6b0c82dc2b0a69
Justin Chow <hrll71990@gmail.com>
2021-01-08 12:30:13 -0800

    found this.state.term bug in Search.jsx, defined Schema, stubbing out front-end POST req

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+import axios from 'axios';
 import Search from './components/Search.jsx';
 import RepoList from './components/RepoList.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       repos: []
     }
-
+    // probably put this.search = this.search.bind(this);
   }
 
   search (term) {
     console.log(`${term} was searched`);
     // TODO
+    axios.post('/repos', term)
+      .then(reponse => {
+        console.lo('POST request sent and this is the response.')
+      })
   }
 
   render () {
--- database/index.js
+++ database/index.js
@@ -3,6 +3,14 @@ mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+
+  // name: repo name
+  name: String,
+  user: String,
+  stargazers: Number,
+  watchers: Number,
+  forks: Number
+
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
