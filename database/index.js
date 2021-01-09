fcd4b5f03c525dc1ef249743ff639bf1a830d907
Justin Chow <hrll71990@gmail.com>
2021-01-09 12:14:28 -0800

    mongoose.connect should be making a connection to localhost port 27107, using repoDB

--- database/index.js
+++ database/index.js
@@ -1,18 +1,24 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
-
+mongoose.connect('mongodb://localhost:27017/repoDB');
+// this is connecting to localhost but no port is given
+// goes to "fetcher" db, probably make something different
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
 
-  name: String,
+  name: {type: String, unique: true},
   stargazers: Number,
   watchers: Number,
   forks: Number
 
 });
 
+
+// Models: like Classes - can create new instances of them using "new Class"
+// instances of Models are the actual documents that live in db
+
+// when collection is created,
 // Mongoose automatically looks for lowercase, plural version of
-// given Model name (i.e. "repos" down below)
+// given Model name (i.e. collection "repos" down below should live in "fetcher" db)
 let Repo = mongoose.model('Repo', repoSchema);
 
 let save = (repos) => {
@@ -26,6 +32,6 @@ let save = (repos) => {
   // db.repos.insert( { name: "user/repo_name", stargazers: 5, watchers: 3, forks: 0 } )
 }
 
-//let newRepo = new Repo({name: "Hello World"}).save();
+//let newRepo = new Repo({name: "Hello World1", stargazers: 2, watchers: 3, forks: 0}).save()
 
 module.exports.save = save;
